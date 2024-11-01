c7f8c86cb23b2d448f218b9c046e09b5ed64c49c
Sam <samuel.fitz@btinternet.com>
2024-11-01 20:49:04 +1300

    style: updating the header component

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,7 +1,21 @@
 import styles from './header.module.scss';
 
 const Header = () => {
-  return <header className={styles.header}>Header</header>;
+  return (
+    <header className={styles.header}>
+      {/* Search Bar */}
+      <div className='header__toolbar'>
+        <div className='relative flex h-min w-[200px]'>
+          <Search className='absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white' />
+          <input
+            className='w-full rounded border-none bg-gray-100 p-2 pl-8 placeholder-gray-500 focus:border-transparent focus:outline-none dark:bg-gray-700 dark:text-white dark:placeholder-white'
+            type='search'
+            placeholder='Search...'
+          />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
